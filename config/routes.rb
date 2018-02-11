2af262956764e2f7c345528e2c5628acaa10dd18
mopinfish <mopinfish@users.noreply.github.com>
2018-02-11 11:16:40 +0900

    Introduce mapbox (#1)
    * [open-data] add mapbox template and routing
    
    * [open-data] refactor script to another file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/about',   to: 'home#index'
   get '/contact', to: 'home#index'
+  get '/map',     to: 'map#index'
 
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
