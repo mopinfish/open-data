21757e3147db499d9381249ec23faf8a9fb73b10
shotaATF <ogano.shota.atf.development@gmail.com>
2018-02-11 12:44:49 +0900

    Add map view

--- app/javascript/packs/components/map.vue
+++ app/javascript/packs/components/map.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <ul id="example">
+  <div id='map' style='width: 800px; height: 600px;'>
+  </div>
+  <div>
+    <ul id="bus">
       <li v-for="busStop in busStops">
         {{ busStop }}
       </li>
     </ul>
   </div>
+</div>
 </template>
 
 <script>
@@ -18,9 +22,19 @@
       }
     },
     mounted() {
+      this.loadMap();
       this.fetchBusStops();
     },
     methods: {
+      loadMap: function () {
+          mapboxgl.accessToken = '';
+          var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v10',
+          center: [139.6, 35.6],
+          zoom: 9
+        });
+      },
       fetchBusStops: function () {
         axios.get('https://api-tokyochallenge.odpt.org/api/v4/odpt:BusstopPole?acl:consumerKey=').then((response) => {
           for(var i = 0; i < response.data.length; i++) {
