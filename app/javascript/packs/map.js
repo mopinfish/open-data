ebfdfb2642f39c08066b84c76b52ce7f7f521f5c
OtsukaNoboru <mopinfish@gmail.com>
2018-02-11 10:58:56 +0900

    [open-data] refactor script to another file

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,2 +1,7 @@
-import axios from 'axios';
-console.log(axios);
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9waW5maXNoIiwiYSI6ImNqZGZreGVuajBhNjIyd29idXI3ZHFxNm4ifQ._edxUwp0j5sAl7FsK9oyrA';
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
