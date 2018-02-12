22feadb10aa2255ffa7122769e5df151ece7928c
OtsukaNoboru <mopinfish@gmail.com>
2018-02-12 20:05:31 +0900

    [open-data] refactor api to server side

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root            to: 'home#index'
   get '/contact', to: 'home#index'
   get '/about',   to: 'home#index'
+  get '/api/v4/:resource',   to: 'api#show'
 end
