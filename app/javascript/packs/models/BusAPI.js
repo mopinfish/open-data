00b8f2900cf323281a31ea32c9f34879f1c42a14
OtsukaNoboru <mopinfish@gmail.com>
2018-02-12 20:11:08 +0900

    [open-data] refact front ajax class

--- app/javascript/packs/models/BusAPI.js
+++ app/javascript/packs/models/BusAPI.js
@@ -1,27 +1,26 @@
 import axios from 'axios';
 
 export default class {
-  static URL_BASE = 'https://api-tokyochallenge.odpt.org/api/v4';
-  static ACCESS_TOKEN = '10b75365528b32fce90ec5024a35fa9797a045568821225f779aeaf054d114fb';
+  static URL_BASE = '/api/v4';
 
   static getBusPolesByOperator(operator) {
     let token = this.ACCESS_TOKEN;
     let resource = '/odpt:BusstopPole'
     let query = 'odpt:operator'
-    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator + '&' + 'acl:consumerKey=' + token)
+    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator)
   }
 
   static getBusRoutePatternsByOperator(operator) {
     let token = this.ACCESS_TOKEN;
     let resource = '/odpt:BusroutePattern'
     let query = 'odpt:operator'
-    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator + '&' + 'acl:consumerKey=' + token)
+    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator)
   }
 
   static getBusByOperator(operator) {
     let token = this.ACCESS_TOKEN;
     let resource = '/odpt:Bus'
     let query = 'odpt:operator'
-    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator + '&' + 'acl:consumerKey=' + token)
+    return axios.get(this.URL_BASE + resource + '?' + query + '=' + operator)
   }
 }
