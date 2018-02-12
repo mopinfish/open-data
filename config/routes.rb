03ef001e8b18a5c5da282ad8e0ac50a962def408
OtsukaNoboru <mopinfish@gmail.com>
2018-02-12 10:52:10 +0900

    [open-data] reverse routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-#  root to: 'home#index'
-#  get '/about',   to: 'home#index'
-#  get '/contact', to: 'home#index'
+  root to: 'home#index'
+  get '/about',   to: 'home#index'
+  get '/contact', to: 'home#index'
   get '/map',     to: 'map#index'
   get '/map/terminal', to: 'map#terminal'
-#  get '/bus_map', to: 'home#index'
+  get '/bus_map', to: 'home#index'
 
-#  namespace :api, format: 'json' do
-#    resources :tasks, only: [:index, :create, :update]
-#  end
+  namespace :api, format: 'json' do
+    resources :tasks, only: [:index, :create, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
